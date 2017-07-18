0131b9bff208ac038ffc3b01d0ea2c6ecb6f3fad
Zac Barnard <barnard.zac@gmail.com>
2017-07-17 23:29:57 -0500

    add tests for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'foo/bar'
+
   get 'static_pages/home'
-
   get 'static_pages/help'
-
+  get 'static_pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
